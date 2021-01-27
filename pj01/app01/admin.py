9c5b3bffe282415b3d1b7a883687bf8c3e3ae074
Jheng,Ming-Yan <44049521+Mingyan0319@users.noreply.github.com>
2021-01-27 21:59:54 +0800

    Update admin.py

--- pj01/app01/admin.py
+++ pj01/app01/admin.py
@@ -5,6 +5,8 @@ from django.db.models import Count
 from django.db.models.functions import TruncDay
 from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 from django.core import serializers
+from django.http import JsonResponse
+from django.urls import path
 # Register your models here.
 
 from  .models import DHT22
@@ -19,12 +21,29 @@ class DHT22Admin(admin.ModelAdmin):
     
     def changelist_view(self, request, extra_context=None):
         # Aggregate new subscribers per day
-        chart_data = DHT22.objects.all()
-            
-        
+        chart_data = self.chart_data()
+           
         as_json = serializers.serialize('json', list(chart_data))
         extra_context = extra_context or {"chart_data": as_json}
         
         return super().changelist_view(request, extra_context=extra_context)
+    
+    def get_urls(self):
+        urls = super().get_urls()
+        extra_urls = [
+            path("chart_data/", self.admin_site.admin_view(self.chart_data_endpoint))
+        ]
+        # NOTE! Our custom urls have to go before the default urls, because they
+        # default ones match anything.
+        return extra_urls + urls
+    
+    def chart_data_endpoint(self, request):
+        chart_data = self.chart_data()
+        return JsonResponse(list(chart_data), safe=False)
+
+    def chart_data(self):
+        return (
+            DHT22.object.all()
+        )
     
 admin . site . register ( DHT22 , DHT22Admin)
