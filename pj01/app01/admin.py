f94ac3744cb039cbbeaa1264b0a06bf49b16dab4
Jheng,Ming-Yan <44049521+Mingyan0319@users.noreply.github.com>
2021-02-20 17:08:14 +0800

    Update admin.py

--- pj01/app01/admin.py
+++ pj01/app01/admin.py
@@ -1,49 +1,42 @@
 from django.contrib import admin
-import json
-from django.core.serializers.json import DjangoJSONEncoder
-from django.db.models import Count
-from django.db.models.functions import TruncDay
-from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
+from django.contrib.admin.views.main import ChangeList
 from django.core import serializers
-from django.http import JsonResponse
 from django.urls import path
-# Register your models here.
-
-from  .models import DHT22
-
-
+from django.http import HttpResponse
+ 
+from rangefilter.filter import DateTimeRangeFilter
+ 
+from .models import DHT22
+ 
+class TimeChangeList(ChangeList):
+    def get_results(self, request):
+        super().get_results(request)
+        self.chart_data = serializers.serialize('json', list(self.result_list.all()))
+ 
 class DHT22Admin(admin.ModelAdmin):
-
     list_display = ('temp', 'humidity', 'time')
-    list_filter = ( ('time', DateTimeRangeFilter),)
+    list_filter = (('time', DateTimeRangeFilter), )
     search_fields = ['time']
-    
-    
+    __ajax_get_chart_data_path = 'chart_data/'
+ 
     def changelist_view(self, request, extra_context=None):
-        # Aggregate new subscribers per day
-        chart_data = self.chart_data()
-           
-        as_json = serializers.serialize('json', list(chart_data))
-        extra_context = extra_context or {"chart_data": as_json}
-        
+        extra_context = extra_context or {'ajax_get_chart_data_path': self.__ajax_get_chart_data_path}
+ 
         return super().changelist_view(request, extra_context=extra_context)
-    
+ 
     def get_urls(self):
         urls = super().get_urls()
         extra_urls = [
-            path("chart_data/", self.admin_site.admin_view(self.chart_data_endpoint))
+            path(self.__ajax_get_chart_data_path, self.admin_site.admin_view(self.__ajax_get_chart_data))
         ]
-        # NOTE! Our custom urls have to go before the default urls, because they
-        # default ones match anything.
         return extra_urls + urls
-    
-    def chart_data_endpoint(self, request):
-        chart_data = self.chart_data()
-        return JsonResponse(list(chart_data), safe=False)
-
-    def chart_data(self):
-        return (
-            DHT22.objects.all()
-        )
-    
-admin . site . register ( DHT22 , DHT22Admin)
+ 
+    def get_changelist(self, request):
+        return TimeChangeList
+ 
+    def __ajax_get_chart_data(self, request):
+        cl = self.get_changelist_instance(request)
+        cl.get_results(request)
+        return HttpResponse(cl.chart_data)
+ 
+admin.site.register(DHT22, DHT22Admin)
